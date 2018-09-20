b4fda1beb7937e5b1ca0f58e586665a7364ac3bb
Ihar Petrushenka <petrushen@yahoo.com>
2018-09-20 13:06:45 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,62 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
   }
-
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
-
   at(index) {
-    // your implementation
+    return this.array[index];
   }
-
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+   return this.array;
   }
 
   sort(indices) {
-    // your implementation
-  }
 
+      let new_array = []; 
+      let k = 0;
+      let newindices;
+    
+      if(indices){
+        newindices = indices.sort(numberComparison);
+      } else {
+        return "No elements are passed to sort.";
+      }
+ 
+
+      while(k<newindices.length)
+      {
+        new_array.push(this.array[newindices[k]]);
+        k++;
+    }
+ 
+    switch(this.comparator){
+      case undefined:
+      new_array = new_array.sort(numberComparison);
+      break;
+      default:
+      new_array = new_array.sort(this.comparator);
+    }
+
+    let j = 0; 
+
+    while(j < new_array.length){
+      this.array[newindices[j]] = new_array[j];
+      j++;
+    }   
+
+    function numberComparison(x,y) {
+      return x - y;
+    }
+      return this.array;
+  }
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
-
 module.exports = Sorter;
